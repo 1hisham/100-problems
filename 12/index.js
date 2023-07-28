f8008143b04cbf3eaecd104cf46614c9783dcaee
1hisham <hisham1ahmmed@gmail.com>
2023-07-28 15:30:12 +0530

    add solution for qs:no-12

--- 12/index.js
+++ 12/index.js
@@ -1,50 +1,76 @@
 let classObj = {
-    "name" : "class A" ,
-    "teacherName" : "Mary" ,
-    "students" : [ 
-        {
-            "name" : "Ravi" ,
-            "id" : "101" ,
-            "marks" : [
-                {"subject" : "English" , "mark" : 25}, 
-                {"subject" : "Maths", "mark" : 48}, 
-                {"subject" : "Physics", "mark" : 40}, 
-                {"subject" : "Chemistry", "mark" : 30},
-                {"subject" : "Computer", "mark" : 20}
-            ]
-        },
-        {
-            "name" : "Aju" ,
-            "id" :  "102",
-            "marks" : [
-                {"subject" : "English" , "mark" : 35}, 
-                {"subject" : "Maths", "mark" : 38}, 
-                {"subject" : "Physics", "mark" : 33}, 
-                {"subject" : "Chemistry", "mark" : 34},
-                {"subject" : "Computer", "mark" : 30}
-            ]
-        },
-        {
-            "name" : "Mini SS" ,
-            "id" : "103" ,
-            "marks" : [
-                {"subject" : "English" , "mark" : 12}, 
-                {"subject" : "Maths", "mark" : 49}, 
-                {"subject" : "Physics", "mark" : 18}, 
-                {"subject" : "Chemistry", "mark" : 30},
-                {"subject" : "Computer", "mark" : 40}
-            ]
-        },
-        {
-            "name" : "Binu" ,
-            "id" : "104" ,
-            "marks" : [
-                {"subject" : "English" , "mark" : 49}, 
-                {"subject" : "Maths", "mark" : 49}, 
-                {"subject" : "Physics", "mark" : 47}, 
-                {"subject" : "Chemistry", "mark" : 46},
-                {"subject" : "Computer", "mark" : 50}
-            ]
-        }
-    ]
-}    
+  name: "class A",
+  teacherName: "Mary",
+  students: [
+    {
+      name: "Ravi",
+      id: "101",
+      marks: [
+        { subject: "English", mark: 25 },
+        { subject: "Maths", mark: 48 },
+        { subject: "Physics", mark: 40 },
+        { subject: "Chemistry", mark: 30 },
+        { subject: "Computer", mark: 20 },
+      ],
+    },
+    {
+      name: "Aju",
+      id: "102",
+      marks: [
+        { subject: "English", mark: 35 },
+        { subject: "Maths", mark: 38 },
+        { subject: "Physics", mark: 33 },
+        { subject: "Chemistry", mark: 34 },
+        { subject: "Computer", mark: 30 },
+      ],
+    },
+    {
+      name: "Mini SS",
+      id: "103",
+      marks: [
+        { subject: "English", mark: 12 },
+        { subject: "Maths", mark: 49 },
+        { subject: "Physics", mark: 18 },
+        { subject: "Chemistry", mark: 30 },
+        { subject: "Computer", mark: 40 },
+      ],
+    },
+    {
+      name: "Binu",
+      id: "104",
+      marks: [
+        { subject: "English", mark: 49 },
+        { subject: "Maths", mark: 49 },
+        { subject: "Physics", mark: 47 },
+        { subject: "Chemistry", mark: 46 },
+        { subject: "Computer", mark: 50 },
+      ],
+    },
+  ],
+};
+// Write a function to find and print the student with the highest total marks.
+
+function studentWithHighestMark(classObj) {
+  const studentHighestMark = []
+  let higestMark = 0;
+  classObj.students.forEach((student) => {
+    let totalMark = 0;
+    student.marks.forEach((items) => {
+        totalMark += items.mark
+    });
+    if(totalMark > higestMark){
+        higestMark = totalMark
+    }
+  });
+  classObj.students.forEach((student) => {
+    let totalMark = 0;
+    student.marks.forEach((items) => {
+        totalMark += items.mark
+    });
+    if(totalMark === higestMark){
+        studentHighestMark.push(student.name)
+    }
+  });
+  return studentHighestMark
+}
+console.log(studentWithHighestMark(classObj));
